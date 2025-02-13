e56050a786ec6cff82b94cf742981a7e8e013e75
Chris <chriskareh@proton.me>
2025-02-13 07:07:20 -0500

    stars mobile fix

--- src/Components/StarBackground.jsx
+++ src/Components/StarBackground.jsx
@@ -4,11 +4,15 @@ import './StarBackground.css';
 export default function StarBackground() {
   return (
     <>
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-      <div id="stars4"></div>
-      <div id="stars5"></div>
+      <div className='container'>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="stars4"></div>
+        <div id="stars5"></div>
+        {/* NEW: Left side stars */}
+        <div id="starsLeft"></div>
+        </div>
     </>
   );
 }
